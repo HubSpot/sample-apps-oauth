02468b135d1509c1d5a782dcd41d9e13fcdda87f
Pablo <pablo.jimenez@exheus.com>
2024-04-19 14:41:09 +0200

    fix: the correct get_auth_url attribute is 'scope' not 'scopes'

--- python/app/routes/oauth.py
+++ python/app/routes/oauth.py
@@ -15,7 +15,7 @@ def login():
 @module.route("/authorize")
 def authorize():
     auth_url = get_auth_url(
-        scopes=("crm.objects.contacts.read",),
+        scope=("crm.objects.contacts.read",),
         client_id=os.environ.get("HUBSPOT_CLIENT_ID"),
         redirect_uri=get_redirect_uri(),
     )
